0e2a3c88cb8279a753d9938d0ffc67cc321f73b7
matt <mjahalden@gmail.com>
2024-06-12 10:15:57 -0700

    Added frame filtering & object detection functionality

--- main.py
+++ main.py
@@ -9,15 +9,48 @@ video = cv2.VideoCapture(0)
 # Provide webcam time to load
 time.sleep(1)
 
+# Define variable for baseline frame
+first_frame = None
+
 while True:
     check, frame = video.read()
 
+    # Convert to greyscale to process less color data/arrays
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2. GaussianBlur(gray_frame, (21, 21), 0)
+
     # Output our video
-    cv2.imshow("My video", frame)
-    print(frame)
+    #cv2.imshow("My video", gray_frame_gau)
+
+    # Assign first frame on first run through loop to first frame variable
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    # Get Delta of frames we are comparing
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+    # Output delta frame
+    cv2.imshow("My video", delta_frame)
+
+    # Classify pixel value of 30 or higher, set to 255. [1] --> get second item in the list
+    # We want to avoid getting lots of white areas in the output to make this simpler so adjust as needed
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    # More iterations, more processing
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My video", dil_frame)
+
+    # Draw contours around our image so we can outline a box image we will capture
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        # Run loop again if fake object
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Video", frame)
 
     key = cv2.waitKey(1)
-
     # Press q to end the program from your keyboard!
     if key == ord("q"):
         break
@@ -37,5 +70,3 @@ time.sleep(1)
 check3, frame3 = video.read()
 time.sleep(1)
 """
-
-print(frame3)
